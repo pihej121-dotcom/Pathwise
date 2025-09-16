06294f9370be3d966b076a87ec431d1b625423c1
pihej121-dotcom <pihej121@gmail.com>
2025-09-16 15:36:01 -0400

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,16 +1,9 @@
-import { Pool } from 'pg';                           
-import { drizzle } from 'drizzle-orm/node-postgres'; 
-import * as schema from "@shared/schema";
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
-}
-
-export const pool = new Pool({ 
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: { rejectUnauthorized: false }, // required for Railway
 });
 
-export const db = drizzle({ client: pool, schema });
+export const db = drizzle(pool);
